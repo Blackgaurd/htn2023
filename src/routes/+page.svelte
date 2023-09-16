9c645d0cf25835e9f80916ea341558445055b33a
Bryan Deng <bryan.deng002@gmail.com>
2023-09-16 01:32:03 -0400

    centered a div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-row">
+	<div class="flex justify-center items-center h-screen">
+        <!-- center this div vertically -->
+		<div class="m-4">
+            <h1>Find your illness and find the cure!</h1>
+            <div class="flex flex-row bg-gray-500 py-2 px-3 space-x-2 rounded-md">
+                <button class="bg-inherit">&#128269;</button>
+                <input
+                    class="w-[32rem] bg-inherit focus:outline-none text-white"
+                    type="text"
+                    placeholder="Tell us about your symptoms"
+                />
+            </div>
+        </div>
+	</div>
+
+	<div>
+		<p>This is where the output is</p>
+	</div>
+</div>
